406b81596dfdbb7e0fdbcc1bb0b16eda4bb186ab
Valentin-Alexandru0099 <dumitru.valentin.alexandru0@gmail.com>
2021-12-17 11:20:27 -0800

    login-register startups

--- main.py
+++ main.py
@@ -11,5 +11,20 @@ def main_page():
     return render_template("index.html")
 
 
+@app.route("/register")
+def register():
+    return render_template("register-login.html")
+
+@app.route("/login")
+def login():
+    return render_template("register-login.html")
+
+
+app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("main_page"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
