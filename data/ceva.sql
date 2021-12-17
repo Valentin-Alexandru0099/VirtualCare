27f0c6236e605e56835ba042d37a9c013f701043
ClaudiuOlr <ionutclaudiuolaru@gmail.com>
2021-12-17 16:39:52 +0200

    Add Doctors

--- data/ceva.sql
+++ data/ceva.sql
@@ -10,7 +10,8 @@ CREATE TABLE doctors(
     name text DEFAULT '',
     specialization text DEFAULT '',
     contact varchar NOT NULL,
-    email varchar UNIQUE
+    email varchar UNIQUE,
+    username text DEFAULT ''
 );
 
 CREATE TABLE patients(
@@ -19,7 +20,8 @@ CREATE TABLE patients(
     contact varchar NOT NULL,
     age integer,
     disease varchar NOT NULL,
-    surgery varchar NOT NULL
+    surgery varchar NOT NULL,
+    email varchar UNIQUE
 );
 
 CREATE TABLE disease(
@@ -33,3 +35,8 @@ INSERT INTO disease (id, name, detail) VALUES (2, 'Depression', 'Depression (als
 INSERT INTO disease (id, name, detail) VALUES (3, 'Oral Cancer', 'Oral Cancer, also known as lip and oral cavity carcinoma, is related to oral leukoplakia and leukoplakia, and has symptoms including snoring and halitosis. An important gene associated with Oral Cancer is MIR31 (MicroRNA 31), and among its related pathways/superpathways are Integrated Breast Cancer Pathway and Spinal Cord Injury.');
 INSERT INTO disease (id, name, detail) VALUES (4, 'Ovarian Cancer', 'Ovarian cancer is a disease that affects women. In this form of cancer, certain cells in the ovary become abnormal and multiply uncontrollably to form a tumor. The ovaries are the female reproductive organs in which egg cells are produced. In about 90 percent of cases, ovarian cancer occurs after age 40, and most cases occur after age 60.');
 INSERT INTO disease (id, name, detail) VALUES (5, 'Heart Disease', 'If you are like most people, you think that heart disease is a problem for others. But heart disease is the number one killer in the U.S. It is also a major cause of disability. There are many different forms of heart disease.');
+
+INSERT INTO doctors (id, name, specialization, contact, email, username) VALUES (1, 'Dr. Oancea', 'Orthopedist', '0742180108', 'doctor.oancea@gmail.com', 'Oancea');
+INSERT INTO doctors (id, name, specialization, contact, email, username) VALUES (2, 'Dr. Ali', 'Neurologist', '0770837460', 'doctor.ali@gmail.com', 'Ali');
+INSERT INTO doctors (id, name, specialization, contact, email, username) VALUES (3, 'Dr. Jhony', 'Gainecologist', '0732878234', 'doctor.jhony@gmail.com', 'Jhony');
+INSERT INTO doctors (id, name, specialization, contact, email, username) VALUES (4, 'Dr. Schiopu', 'Dermatologist', '0765753159', 'doctor.schiopu@gmail.com', 'Shiopu');
